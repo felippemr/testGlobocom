14894b33d6a560d13fbd91b03cd174028a2cd46b
Felippe <raposo.felippe@gmail.com>
2014-02-08 22:17:18 -0200

    Some optimizations

--- app/models/application.rb
+++ app/models/application.rb
@@ -7,5 +7,8 @@ class Application
   field :_id, type: String, default: ->{ name }
 
   validates_presence_of :name
-  validates_uniqueness_of :name
+
+  has_many :machines, :inverse_of => :applications, :foreign_key => :application_ids
+
+  index({ _id: 1 }, { unique: true, name: "_id_index" })
 end
--- app/models/machine.rb
+++ app/models/machine.rb
@@ -5,10 +5,9 @@ class Machine
   field :name, type: String
   field :location, type: String
 
-  has_and_belongs_to_many :applications, inverse_of: nil , index: true
-  index({ name: 1 }, { unique: true, name: "name_index" })
+  validates_presence_of   :name, :location
 
-  validates_presence_of   :name
-  validates_uniqueness_of :name
-  validates_presence_of   :location
+  has_and_belongs_to_many :applications, inverse_of: nil , index: true
+
+  index({ _id: 1 }, { unique: true, name: "_id_index" })
 end
