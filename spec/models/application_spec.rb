1e4ae9153c11dbee9e1991a6dec9e5aed6f5d9f5
Felippe <raposo.felippe@gmail.com>
2014-02-09 17:53:51 -0200

    Adds tests to models

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Application do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Associations" do
+    it { should have_many(:machines).as_inverse_of(:applications).with_foreign_key(:application_ids) }
+  end
+
+  context "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+  end
+
+  context "Indexes" do
+  	it { should have_index_for(_id: 1).with_options(unique: true) }
+  	it { should have_index_for(name: 1).with_options(unique: true) }
+  end
+
+  context "Timestamps" do
+  	it { should be_timestamped_document }
+  end
+
+  context "Fields" do
+    it { should have_fields(:type, :_id) }
+  end
 end
--- spec/models/machines_spec.rb
+++ spec/models/machines_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Machine do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context "Associations" do
+    it { should have_and_belong_to_many(:applications) }
+  end
+
+  context "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:location) }
+    it { should validate_uniqueness_of(:name) }
+  end
+
+  context "Indexes" do
+  	it { should have_index_for(_id: 1).with_options(unique: true) }
+    it { should have_index_for(application_ids: 1).with_options(background: true) }
+  end
+
+  context "Timestamps" do
+  	it { should be_timestamped_document }
+  end
+
+  context "Fields" do
+    it { should have_fields(:_id) }
+  end
 end
