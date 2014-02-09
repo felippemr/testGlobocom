82e2883f606e24815437f1dfb8ffac3c14ee784d
Felippe <raposo.felippe@gmail.com>
2014-02-09 17:57:23 -0200

    Adds tests to models

---
+++ spec/api/v1/applications_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "/api/v1/applications", :type => :api do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
---
+++ spec/api/v1/machines_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "/api/v1/machines", :type => :api do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
